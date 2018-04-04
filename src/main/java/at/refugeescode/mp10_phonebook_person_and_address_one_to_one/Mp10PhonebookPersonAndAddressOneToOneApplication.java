1ef3b78d6ba4c17e0ccd792f5f3f2f344cd52bec
Hadi <hadi.effatian@gmail.com>
2018-04-04 18:48:48 +0200

    Created Project

---
+++ src/main/java/at/refugeescode/mp10_phonebook_person_and_address_one_to_one/Mp10PhonebookPersonAndAddressOneToOneApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.mp10_phonebook_person_and_address_one_to_one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp10PhonebookPersonAndAddressOneToOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mp10PhonebookPersonAndAddressOneToOneApplication.class, args);
+	}
+}
---
+++ src/test/java/at/refugeescode/mp10_phonebook_person_and_address_one_to_one/Mp10PhonebookPersonAndAddressOneToOneApplicationTests.java
@@ -0,0 +1,16 @@
+package at.refugeescode.mp10_phonebook_person_and_address_one_to_one;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Mp10PhonebookPersonAndAddressOneToOneApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
